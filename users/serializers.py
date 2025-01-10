e30a7a25a6839ac3a232315371fd7977cd1d01ed
talosPy <tala0147@gmail.com>
2025-01-10 09:25:59 +0200

    added custom user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,26 @@
-from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import LeagueUser
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims based on LeagueUser fields
+        token['username'] = user.username
+        token['isAdmin'] = user.is_staff  # Adjust this field if `isAdmin` differs in LeagueUser
+
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # Include additional fields in the response
+        user = self.user  # The authenticated user
+        data['username'] = user.username
+        data['isAdmin'] = user.is_staff  # Adjust this based on your model
+        return data
+
 
 class LeagueUserSerializer(serializers.ModelSerializer):
     class Meta:
